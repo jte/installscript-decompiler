93240676069bc01a630a6f76fd311745c594717b
Dominik Batusic <dominik.batusic@gmail.com>
2024-02-06 18:08:53 +0100

    Remove lib file parsing for now

--- src/InstallShield/Wrapper/LibFile.cpp
+++ src/InstallShield/Wrapper/LibFile.cpp
@@ -2,29 +2,5 @@
 
 LibFile::LibFile(StreamPtr& filePtr)
 {
-	uint32_t Signature = 0;
-	filePtr.Read(Signature);
-	if (Signature != 'AdOp')
-	{
-		throw std::runtime_error("Invalid signature for library file. Expected 'AdOp'");
-	}
 
-	uint32_t unknown = 0;
-	filePtr.Read(unknown);
-	if (unknown != 1)
-	{
-		throw std::runtime_error("Library header member 'unknown' is not set to value 1.");
-	}
-
-	uint32_t numScripts = 0;
-	filePtr.Read(numScripts);
-
-	for (size_t i = 0; i < numScripts; ++i)
-	{
-		ScriptInfo s;
-		s.name = filePtr.ReadInsString();
-		filePtr.Read(s.offset);
-		filePtr.Read(s.length);
-		m_scripts.push_back(s);
-	}
 }
