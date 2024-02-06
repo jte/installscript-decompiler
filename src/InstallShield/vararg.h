66d922bc3add7c3263afb87a2d68ac975c66e384
Dominik Batusic <dominik.batusic@gmail.com>
2024-02-06 18:37:17 +0100

    Fix clang warnings #3

--- src/InstallShield/vararg.h
+++ src/InstallShield/vararg.h
@@ -35,22 +35,15 @@ public:
 		}
 		ss << (m_address <= -100 ? (m_address+100)*-1 : m_address);
 		return new VariableExpression(ss.str());
-
-		//ss << (parameter ? "p" : "v") << ' ' << type << ' ' << m_address;
-		//return new VariableExpression(ss.str());
 	}
 	vararg(StreamPtr& filePtr)
 	{
 		Parse(filePtr);
 		m_type = type;
 	}
-	void Parse(StreamPtr& filePtr)
+	void Parse(StreamPtr& filePtr) override
 	{
 		filePtr.Read(m_address);
-		//if (static_cast<int16_t>(m_address) <= -100)
-		//{
-		//	m_address = static_cast<uint16_t>(m_address + 100);
-		//}
 	}
 	int16_t GetAddress() const
 	{
