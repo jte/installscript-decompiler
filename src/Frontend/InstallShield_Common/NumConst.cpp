7d8ff594342846fa4220e187833ff84e19d72b36
Dominik Batusic <dominik.batusic@gmail.com>
2024-02-21 16:59:55 +0100

    Trying to fix compile errors with clang & gcc Part 1

--- src/Frontend/InstallShield_Common/NumConst.cpp
+++ src/Frontend/InstallShield_Common/NumConst.cpp
@@ -1,23 +1,2 @@
 #include "NumConst.h"
 #include <string>
-
-CNumConst::CNumConst(StreamPtr& filePtr)
-{
-	Parse(filePtr);
-	m_type = ArgType::NumArg;
-}
-
-void CNumConst::Parse(StreamPtr& filePtr)
-{
-	filePtr.Read(m_num);
-}
-
-int32_t CNumConst::GetConst() const
-{
-	return m_num;
-}
-
-void CNumConst::print(std::ostream& os) const
-{
-	os << GetConst();
-}
--- src/Frontend/InstallShield_Common/NumConst.h
+++ src/Frontend/InstallShield_Common/NumConst.h
@@ -9,13 +9,26 @@ class CNumConst : public IArgument
 private:
 	int32_t m_num;
 protected:
-	void print(std::ostream& os) const override;
+	void print(std::ostream& os) const override
+	{
+		os << GetConst();
+	}
 	AbstractExpression* ToExpression(SymbolTable* symTable) const override
 	{
 		return new NumberExpression(m_num);
 	}
 public:
-	CNumConst(StreamPtr& filePtr);
-	void Parse(StreamPtr& filePtr) override;
-	int32_t GetConst() const;
+	CNumConst(StreamPtr& filePtr)
+	{
+		Parse(filePtr);
+		m_type = ArgType::NumArg;
+	}
+	void Parse(StreamPtr& filePtr) override
+	{
+		filePtr.Read(m_num);
+	}
+	int32_t GetConst() const
+	{
+		return m_num;
+	}
 };
